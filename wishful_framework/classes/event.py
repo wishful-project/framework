18b53d3f00a110d72dea350b172b8f16f3caa97e
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-03-08 11:08:24 +0100

    update for rules

--- wishful_framework/classes/event.py
+++ wishful_framework/classes/event.py
@@ -7,21 +7,31 @@ __email__ = "{gawlowicz, zubow}@tkn.tu-berlin.de"
 
 
 class Event(object):
-    def __init__(self):
+    def __init__(self, etype):
+        self.type = etype
         pass
 
 
 class TimeEvent(Event):
-    def __init__(self, func, args=(), interval=1, iface=None):
-        super(TimeEvent, self).__init__()
-        self.upi_type = func.__module__
+    def __init__(self, func, args=(), kwargs={}, interval=1, iface=None):
+        super(TimeEvent, self).__init__("TimeEvent")
+        upiType = func.__module__
+        upiType = upiType.split(".")
+        upiType = upiType[len(upiType)-1]
+        self.upi_type = upiType
         self.upi_func = func.__name__
-        self.args = args
+        
+        self.kwargs = {}
+        if args:
+            self.kwargs["args"] = args
+        if kwargs:
+            self.kwargs["kwargs"] = kwargs
+
         self.interval = interval
         self.iface = iface
 
 
 class PktEvent(Event):
     def __init__(self, iface):
-        super(PktEvent, self).__init__()
+        super(PktEvent, self).__init__("PktEvent")
         self.iface = iface
