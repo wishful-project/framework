ffb20a55a747f3dbae3b336dd522f2cfb6249be7
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-03-15 09:50:12 +0100

    split functionalities

--- wishful_framework/modules/agent_module.py
+++ wishful_framework/modules/agent_module.py
@@ -28,7 +28,7 @@ class AgentModule(WishfulModule):
         dest = msgContainer[0]
         cmdDesc = msgContainer[1]
         kwargs = msgContainer[2]
-        
+
         self.log.debug("Process msg: {}:{}".format(cmdDesc.type, cmdDesc.func_name))
         command = cmdDesc.func_name
 
@@ -90,3 +90,103 @@ class AgentModule(WishfulModule):
         response = [dest, respDesc, retVal]
 
         return response
+
+    def before_call(self, msgContainer):
+        assert len(msgContainer) == 3
+        dest = msgContainer[0]
+        cmdDesc = msgContainer[1]
+        kwargs = msgContainer[2]
+
+        self.log.debug("Before call: {}:{}".format(cmdDesc.type, cmdDesc.func_name))
+        command = cmdDesc.func_name
+        #TODO: check if function is available
+        func = getattr(self, command)
+
+        #set interface before UPI function call, so we can use self.interface in function
+        self.interface = None
+        if cmdDesc.HasField('interface'):
+            self.interface = cmdDesc.interface
+
+        #if first call to module, execute function decorated with before_firtst_call_to_module
+        if self.firstCallToModule:
+            self.firstCallToModule = False
+            self.first_call_to_module()
+
+        #if there is function that has to be called before UPI function, call it
+        if hasattr(func, '_beforeCall'):
+            before_func = getattr(self, func._beforeCall)
+            #execute_function checks if need to run in new thread, and do so if true
+            self.execute_function(before_func)
+
+
+    def execute_call(self, msgContainer):
+        assert len(msgContainer) == 3
+        dest = msgContainer[0]
+        cmdDesc = msgContainer[1]
+        kwargs = msgContainer[2]
+
+        self.log.debug("Execute call: {}:{}".format(cmdDesc.type, cmdDesc.func_name))
+        command = cmdDesc.func_name
+        #TODO: check if function is available
+        func = getattr(self, command)
+
+        #set interface before UPI function call, so we can use self.interface in function
+        self.interface = None
+        if cmdDesc.HasField('interface'):
+            self.interface = cmdDesc.interface
+
+        my_args = ()
+        my_kwargs = {}
+        if kwargs:
+            my_args = kwargs['args']
+            my_kwargs = kwargs['kwargs']
+
+        retVal = None
+        exception = False
+        try:
+            retVal = func(*my_args, **my_kwargs)
+        except Exception as e:
+            self.log.warning("Exception: {}".format(e))
+            exception = True
+            retVal = e
+
+        dest = "controller"
+        respDesc = msgs.CmdDesc()
+        respDesc.type = cmdDesc.type
+        respDesc.func_name = cmdDesc.func_name
+        respDesc.call_id = cmdDesc.call_id
+
+        #TODO: define new protobuf message for return values; currently using repeat_number in CmdDesc 
+        #0-executed correctly, 1-exception
+        if exception:
+            respDesc.repeat_number = 1
+        else:
+            respDesc.repeat_number = 0
+
+        #Serialize return value
+        respDesc.serialization_type = msgs.CmdDesc.PICKLE
+        response = [dest, respDesc, retVal]
+
+        return response
+
+    def after_call(self, msgContainer):
+        assert len(msgContainer) == 3
+        dest = msgContainer[0]
+        cmdDesc = msgContainer[1]
+        kwargs = msgContainer[2]
+
+        self.log.debug("Execute call: {}:{}".format(cmdDesc.type, cmdDesc.func_name))
+        command = cmdDesc.func_name
+        #TODO: check if function is available
+        func = getattr(self, command)
+
+        #set interface before UPI function call, so we can use self.interface in function
+        self.interface = None
+        if cmdDesc.HasField('interface'):
+            self.interface = cmdDesc.interface
+
+        #if there is function that has to be called after UPI function, call it
+        if hasattr(func, '_afterCall'):
+            after_func = getattr(self, func._afterCall)
+            #execute_function checks if need to run in new thread, and do so if true
+            self.execute_function(after_func)
